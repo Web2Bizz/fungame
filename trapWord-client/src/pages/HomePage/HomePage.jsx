025724fd59ab42d5476eb805051f45122b7edce6
xarlein <alexvolkovdd@gmail.com>
2024-06-23 01:18:17 +0400

    feat(front): моя останока

--- trapWord-client/src/pages/HomePage/HomePage.jsx
+++ trapWord-client/src/pages/HomePage/HomePage.jsx
@@ -1,37 +1,39 @@
-import { Button } from 'antd'
-import { useNavigate } from 'react-router-dom'
-import { roomAPI } from '../../entitys/Room/api/service.js'
+import { Button } from "antd";
+import { useNavigate } from "react-router-dom";
+import { roomAPI } from "../../entitys/Room/api/service.js";
 
 const HomePage = () => {
-	const navigate = useNavigate()
-	const [createRoom, {}] = roomAPI.useCreateRoomMutation()
+  const navigate = useNavigate();
+  const [createRoom, {}] = roomAPI.useCreateRoomMutation();
 
-	const onCreateRoomClick = async () => {
-		try {
-			const response = await createRoom()
-			if (response.error) {
-				// Обработка ошибки, если она есть
-				console.error('Ошибка при создании комнаты:', data.roomCode.error)
-			} else {
-				// Переходим на страницу лобби с roomCode, если все в порядке
-				const { roomCode } = response.data
-				navigate(`/lobby/${roomCode}`)
-			}
-		} catch (error) {
-			console.error('Ошибка при создании комнаты:', error)
-		}
-	}
+  const onCreateRoomClick = async () => {
+    try {
+      const response = await createRoom();
+      if (response.error) {
+        // Обработка ошибки, если она есть
+        console.error("Ошибка при создании комнаты:", data.roomCode.error);
+      } else {
+        // Переходим на страницу лобби с roomCode, если все в порядке
+        const { roomCode } = response.data;
+        navigate(`/lobby/${roomCode}`);
+      }
+    } catch (error) {
+      console.error("Ошибка при создании комнаты:", error);
+    }
+  };
 
-	return (
-		<div className='flex items-center flex-col gap-5'>
-			<h1 className='mb-20'>Опасные слова</h1>
+  return (
+    <div className="flex items-center flex-col gap-5">
+      <h1 className="mb-20">Опасные слова</h1>
 
-			<Button type='primary' onClick={onCreateRoomClick}>
-				Создать комнату
-			</Button>
-			<Button type='primary'>Присоединиться</Button>
-		</div>
-	)
-}
+      <Button type="primary" onClick={onCreateRoomClick}>
+        Создать комнату
+      </Button>
+      <Button type="primary">Присоединиться</Button>
+      <h1>Главный разраб на этой свиноферме⚡</h1>
+      <img width={300} src="/1.jpg" />
+    </div>
+  );
+};
 
-export default HomePage
+export default HomePage;
